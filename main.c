a49857284d4217172d7ca40c34f548456e97f76a
Neptunal <110062414+nptnl@users.noreply.github.com>
2023-06-05 09:38:03 -0400

    3D vertices
    they work... almost

--- main.c
+++ main.c
@@ -1,13 +1,39 @@
 #include <stdio.h>
 #include <math.h>
+#include <unistd.h>
 
-static int DIMX = 16;
-static int DIMY = 8;
+static int DIMX = 64;
+static int DIMY = 32;
+static double FOCAL = 32;
 
 typedef struct {
     double x;
     double y;
 } vx2;
+typedef struct {
+    double x;
+    double y;
+    double z;
+} vx3;
+
+vx2 project(vx3 point) {
+    double xs = 0.5 * DIMX;
+    double ys = 0.5 * DIMY;
+    double multi = FOCAL / (point.z + 2*FOCAL);
+    vx2 result = { (point.x - xs) * multi + xs, (point.y - ys) * multi + ys };
+    return result;
+}
+
+void list_vx2(vx2 point) {
+    printf("%f, %f\n", point.x, point.y);
+}
+void list_vx3(vx3 point) {
+    printf("%f, %f, %f\n", point.x, point.y, point.z);
+}
+void list_proj(vx3 point) {
+    vx2 result = project(point);
+    printf("%f, %f\n", result.x, result.y);
+}
 
 void make_spaces(char empty[DIMY][DIMX*2+1]) {
     for (int indy = 0; indy < DIMY; indy ++) {
@@ -19,13 +45,16 @@ void make_spaces(char empty[DIMY][DIMX*2+1]) {
 }
 
 void display(char canvas[DIMY][DIMX*2+1]) {
-    for (int line = DIMY-1; line > 0; line -= 1) {
+    for (int line = DIMY-1; line >= 0; line -= 1) {
         printf("%s\n", canvas[line]);
     }
 }
 
 int within_range(vx2 point) {
-    if (point.x < DIMX && point.y < DIMY) {
+    if (point.x < DIMX
+    && point.y < DIMY
+    && point.x > 0
+    && point.y > 0) {
         return 0;
     }
     return 1;
@@ -36,11 +65,6 @@ void add_point(vx2 point, char canvas[DIMY][DIMX*2+1]) {
     canvas[(int) round(point.y)][(int) round(point.x) * 2] = '[';
     canvas[(int) round(point.y)][(int) round(point.x) * 2 + 1] = ']';
 }
-
-static vx2 coords[] = {
-    { 2., 2. },
-    { 13., 3. },
-};
 
 void grad_line(vx2 c1, vx2 c2, char canvas[DIMY][DIMX*2+1]) {
     double slope = (c2.y - c1.y) / (c2.x - c1.x);
@@ -69,19 +93,46 @@ void make_line(vx2 c1, vx2 c2, char canvas[DIMY][DIMX*2+1]) {
         } else {
             grad_line(c1, c2, canvas);
         }
+        add_point(c2, canvas);
     } else {
         if (diff.y > diff.x) {
             grad_line(c2, c1, canvas);
         } else {
             steep_line(c2, c1, canvas);
         }
+        add_point(c1, canvas);
     }
+}
+
+static vx2 coords[] = {
+    { 2., 2. },
+    { 2., 30. },
+    { 30., 30. },
+    { 30., 2. },
+};
+
+vx3 inc_ref(vx3 point) {
+    double place = (point.x - 16.) - 0.1 * (16. - point.z);
+    point.z += 0.1 * (16. - point.x);
+    point.x = place + 16.;
+    return point;
 }
 
 int main() {
     char canvas[DIMY][DIMX * 2 + 1];
     make_spaces(canvas);
-    make_line(coords[0], coords[1], canvas);
-    display(canvas);
+
+    vx3 c1 = { 20., 2., 4., };
+    vx3 c2 = { 60., 2., 64., };
+
+    while (0 == 0) {
+        c1 = inc_ref(c1);
+        c2 = inc_ref(c2);
+        make_spaces(canvas);
+        make_line(project(c1), project(c2), canvas);
+        display(canvas);
+        usleep(10000);
+    }
+
     return 0;
 }
